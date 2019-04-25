5bac5f61450be36b71236f9d035b4500f678817f
bogodars <bogodar@gmail.com>
2019-04-25 13:24:13 +0300

    Rendering Contentful Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
-
+import blogStyles from './blog.module.scss'
 
 
 
@@ -11,16 +11,12 @@ import Layout from '../components/layout'
 const BlogPage = () => {
 	const data = useStaticQuery(graphql`
 	    query {
-				allMarkdownRemark {
-					edges {
+        allContentfulBlogPost ( sort: { fields: publishedDate, order: DESC }) {
+					edges { 
 						node {
-							frontmatter {
-								title
-								date
-							}
-							fields {
-								  slug
-							}
+							title
+							slug
+							publishedDate(formatString:"MMMM Do, YYYY")
 						}
 					}
 				}
@@ -30,13 +26,13 @@ const BlogPage = () => {
 	return (
 		<Layout>
 			<h1>Blog</h1>
-		  <ol>
-			{data.allMarkdownRemark.edges.map((edge) => {
+		  <ol className={blogStyles.posts}>
+			{data.allContentfulBlogPost.edges.map((edge) => {
 			  return (
-          <li>
-					<Link to={`/blog/${edge.node.fields.slug}`}>
-            <h2>{edge.node.frontmatter.title}</h2>
-            <p>{edge.node.frontmatter.date}</p>
+          <li className={blogStyles.post}>
+					<Link to={`/blog/${edge.node.slug}`}>
+            <h2>{edge.node.title}</h2>
+            <p>{edge.node.publishedDate}</p>
 						</Link>
           </li>
 				)
--- src/posts/gatsby/gatsby.md
+++ src/posts/gatsby/gatsby.md
@@ -3,10 +3,12 @@ title: 'The Great Gatsby Bootcamp'
 date: '2019-04-04'
 ---
 
-I lounch new blog post !
+I launched a new blog post !
+
+![Grass](./grass.png)
 
 # Topics Covered
 
-1. Gatsby
+1. Gatsby X
 2. GraphQL
-3. React
+3. React 
